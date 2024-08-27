d9823e9f9231ab71b7d4c25c2f32f16478c511a2
osoki <osokinkonstantin@mail.ru>
2024-08-27 21:35:45 +0300

    homework

---
+++ src/main/java/com/Employees/list/of/employees/ListOfEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.Employees.list.of.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListOfEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListOfEmployeesApplication.class, args);
+	}
+
+}
